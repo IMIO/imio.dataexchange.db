b38b345a1f2bcbc5bcf24e2a6a296777d25938ca
Martin Peeters <martin.peeters@affinitic.be>
2014-05-23 16:37:47 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+long_description = (
+    open('README.rst').read()
+    + '\n' +
+    open('CHANGES.rst').read()
+    + '\n')
+
+setup(
+    name='imio.dataexchange.db',
+    version=version,
+    description="",
+    long_description=long_description,
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Programming Language :: Python",
+    ],
+    keywords='',
+    author='IMIO',
+    author_email='support@imio.be',
+    url='https://github.com/imio/',
+    license='GPL',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['imio', 'imio.dataexchange'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+)
