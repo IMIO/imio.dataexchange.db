8c597a95125e9c63e0f3801cbd3c8ec4a51f690c
Martin Peeters <martin.peeters@affinitic.be>
2016-02-24 11:02:21 +0100

    Update the file types

--- src/imio/dataexchange/db/scripts/db.sql
+++ src/imio/dataexchange/db/scripts/db.sql
@@ -1,4 +1,5 @@
 DELETE FROM file_type;
-INSERT INTO file_type (id, description) values ('FACT', 'Facture');
 INSERT INTO file_type (id, description) values ('COUR_E', 'Courrier entrant');
 INSERT INTO file_type (id, description) values ('COUR_S', 'Courrier sortant');
+INSERT INTO file_type (id, description) values ('COUR_S_GEN', 'Courrier sortant Généré');
+INSERT INTO file_type (id, description) values ('DELIB', 'Délibération');
